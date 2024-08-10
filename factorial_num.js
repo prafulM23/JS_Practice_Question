056964158d9db8e50e16ea065a63a6664b6d06d9
prafulM23 <prafulm2310@gmail.com>
2024-08-10 23:18:11 +0530

    Add files via upload

---
+++ factorial_num.js
@@ -0,0 +1,14 @@
+// var num = 7;
+// var sum = 1;
+// while (num>0){
+//     sum=sum*num
+//     num--
+// }
+// console.log(sum)
+var num = 10;
+var sum = 1;
+while (num>0){
+    sum=sum*num
+    num--
+}
+console.log(sum)

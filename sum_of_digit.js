7b58051be2ef9c79be20e00f975eea43d6608e61
prafulM23 <prafulm2310@gmail.com>
2024-08-10 23:22:20 +0530

    Add files via upload

---
+++ sum_of_digit.js
@@ -0,0 +1,31 @@
+// var num = 198;
+// var sum = 0 ;
+// while (num>0){
+//     let stor =  num%10 // 154%10=4
+//     sum+=stor // sum = sum+stor // sum = 0+4 = 4
+    
+//     num = Math.floor(num/10) // 154/10 = 15.4 // 15
+    
+// }
+// console.log(num);
+
+// var a = readLineSync.questionInt('enter a num : - ');
+// var sum = 1;
+// while(a>0){
+//     let stor = a%10
+//     sum = sum*stor
+//     a = Math.floor(a/10)
+// }
+// console.log(sum)
+
+// var num = 1;
+// var i = 1;
+// while (i <= 50){
+//     if(i%2==0){
+//         num = num+i // 1+1=2
+//         console.log(num, "sum of",i)
+        
+        
+//      }   
+//       i++
+// }

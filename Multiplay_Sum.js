823a86340731942319f9d5583f9278862a8066d7
prafulM23 <prafulm2310@gmail.com>
2024-08-10 23:20:46 +0530

    Add files via upload

---
+++ Multiplay_Sum.js
@@ -0,0 +1,17 @@
+// let _multiplay = 3;
+// var i = 1;
+// var sum = 0;
+// while(i <= 30){
+//     sum = _multiplay*i
+//     console.log(sum)
+//     i++
+// }
+
+var num = 2;
+var sum = 1;
+var i = 1;
+while (i <= 5){
+    sum = num*sum
+    i++
+}
+console.log(sum)

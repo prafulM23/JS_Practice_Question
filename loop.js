0da8a942ff34b917ca9f2c191bad59eccc7c2dc4
prafulM23 <prafulm2310@gmail.com>
2024-08-10 23:24:07 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,89 @@
+
+const readLineSync = require('readline-sync');
+// let user = readLineSync.question("enter input ; - ")
+// var i = 0;
+// while(i <= 10){
+//     if(user%3 == 0 && user % 5 == 0){
+//         console.log("fizzbuzz")
+//     }else if (user%3==0){
+//         console.log("fizz")
+//     }else if(user%5==0){
+//         console.log("buzz")
+//     }else {
+//         console.log("not divide both")
+//     }
+//     i++
+// }
+
+   
+
+// }
+
+// var a = 3498;
+// var b = Math.floor(a/10)
+// console.log(b)
+
+
+
+// var i = 3;
+// while(i>=1){
+//     console.log(i)
+//     i--
+// }
+
+// var str ="nikhil";
+// var i = 1
+// while(i <= 20){
+//     console.log(i,str)
+//     i++
+
+// } 
+
+// var a = parseInt(readLineSync.question("enter a: -  "));
+// var b = parseInt(readLineSync.question("enetr b : - "))
+// while( a>=0  && b>=0){
+//     a+=b
+//     console.log(a)
+//     break
+// }
+// console.log('this is not postive')
+
+
+
+
+
+
+// var x = readLineSync.question(" enter x : = ")
+// var y = readLineSync.question(" enter exponent : - ");
+// while (  ){
+//     let sum = x**y
+//     console.log(sum)
+//     break
+// }
+
+// var i = 15
+// while(i>0){
+//     console.log(i)
+//     i--
+// }
+
+// var num = 1
+// while ( num <= 30){
+//     if(num%2 != 0){
+//         console.log(num,"is odd")
+//     }
+//     num++
+// }
+
+var str =readLineSync.question("enter a word : - ");
+var length = str.length;
+var i = 1;
+while(i<=length){
+    i++
+}
+console.log(i)
+
+
+
+
+

f39739815ffe6272b66d51214eefdbf482e8bf80
prafulM23 <prafulm2310@gmail.com>
2024-08-10 23:23:43 +0530

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,195 @@
+const readlineSync = require('readline-sync');
+// var a = readlineSync.questionInt('a  : - ');
+// var b = readlineSync.questionInt("b : - ");
+// var c = readlineSync.questionInt("c : - ");
+// var d = readlineSync.questionInt("d : - ");
+
+// if(a>b && a>c && a>d){
+//     if(b>c && b>d){
+//         if(c>d){
+//             console.log(a,b,c,d)
+//         }else{console.log(a,b,d,c)}
+//     }else{
+//         if(c>b && c>d){
+//             if(b>d){
+//                 console.log(a,c,b,d)
+//             }else{
+//                 console.log(a,c,d,b)
+//             }
+//         }else{
+//             if(d>c && d>b){
+//                 if(c>b){
+//                     console.log(a,d,c,b)
+
+//                 }else {
+//                     console.log(a,d,b,c)
+//                 }
+//             }
+//         }
+//     }
+// }else if(b > a && b>c && b>d){
+//     if(a>c && a>d){
+//         if(c>d){
+//             console.log(b,a,c,d)
+//         }else{console.log(b,a,d,c)}
+//     }else{
+//         if(c>a && c>d){
+//             if(a>d){
+//                 console.log(b,c,a,d)
+//             }else { console.log(b,c,d,a)}
+//         }else{
+//             if(d>a && d>c){
+//                 if(a>c){
+//                     console.log(b,d,a,c)
+//                 }else{ console.log(b,d,c,a)}
+//             }
+//         }
+//     }
+// }else if(c>a && c>b && c>d){
+//     if(a>b && a>d){
+//         if(b>d){
+//             console.log(c,a,b,d)
+//         }else{ console.log(c,a,d,b)}
+//     }else{
+//         if(b>a && b>d){
+//             if(a>d){
+//                 console.log(c,b,a,d)
+//             }else{ console.log(c,b,d,a)}
+//         }else{
+//             if(d>a && d>b){
+//                 if(a>b){
+//                     console.log(c,d,a,b)
+//                 }else{ console.log(c,d,b,a)}
+//             }
+//         }
+//     }
+// }else if(d>a && d>b && d>c){
+//     if(a>b && a>c){
+//         if(b>c){
+//             console.log(d,a,b,c)
+//         }else{ console.log(d,a,c,b)}
+//     }else{
+//         if(b>a && b>c){
+//             if(a>c){
+//                 console.log(d,b,a,c)
+//             }else{ console.log(d,b,c,a)}
+//         }else{
+//             if(c>a && c>b){
+//                 if(a>b){
+//                     console.log(d,c,a,b)
+//                 }else{ console.log(d,c,b,a)}
+//             }
+//         }
+//     }
+// }else{
+//     console.log("valid digit")
+// }
+
+// var num1 = readlineSync.questionInt("num1 : - ");
+// var num2 = readlineSync.questionInt("num2 : - ");
+// var num3 = readlineSync.questionInt("num3 : - ");
+
+// if(num1 > num2 && num1>num3){
+//     console.log(num1)
+// }else if(num2>num1 && num2>num3){
+//     console.log(num2)
+// }
+// else if(num3>num1 && num3>num2){
+//     console.log(num3)
+// }
+// else{
+//     console.log(nan)
+// }
+
+
+// var a = 10;
+// a = 11
+// console.log(a)
+
+// let b = 10;
+// let a = 10;
+// console.log(b)
+//  var y = x+y ;
+//  var z = y+4 ;
+//  console.log(z);
+
+// let david = 80;
+// let vinod = 77;
+// let divya = 88;
+// let ishita = 95;
+// let thomas = 68;
+// var marks = readlineSync.question("enter marks : - ");
+
+// if (marks >= 90 && marks <100 ){
+//     console.log("grade A")
+// }else if(marks >= 80 && marks < 90){
+//     console.log('grade B')
+// }else if( marks >= 70 && marks < 80){
+//     console.log("grade C")
+// }else if( marks >= 60 && marks < 70){
+//     console.log("grade D")
+// }else if(marks >= 50 && marks < 60){
+//     console.log("grade F")
+// }else {
+//     console.log("fail")
+// }
+
+
+// let num1 = readlineSync.questionInt("enter num1 : - ");
+// let num2 = readlineSync.questionInt("enter num2 : - ");
+// if(num1 > num2){
+//     console.log('num1 is gretar ')
+// }
+// else 
+//     {console.log("num2 is gretar")}
+
+// let year = readlineSync.question(" enter year : - ");
+// if(year%4==0 || year%400==0){
+//     console.log("leapyear")
+// }
+// else{
+//     console.log("not leapyear")
+// }
+
+// let marks_of_student = readlineSync.question(" enetr marks : - ");
+// if(marks_of_student >=90 && marks_of_student < 100){
+//     console.log("grade S")
+// }
+// else if( marks_of_student>= 80 && marks_of_student< 90){
+//     console.log("grade A")
+// }
+// else if( marks_of_student>= 70 && marks_of_student<80)[
+//     console.log('grade B')
+// ]
+// else if(marks_of_student >= 60 && marks_of_student <70){
+//     console.log("grade C")
+// }
+// else if(marks_of_student >= 50 && marks_of_student<60){
+//     console.log("grade D")
+// }
+// else if(marks_of_student >= 40 && marks_of_student <50){
+//     console.log('grade e')
+// }
+// else{
+//     console.log("fail")
+// }
+
+// var _num = readlineSync.question("enter num : - ");
+// if(_num > 0 || _num < 0){
+//     console.log('this is number')
+// }
+// else{ 
+//     console.log("nan")
+// }
+
+// var number_ = 10;
+// var _user = readlineSync.question("enter code : - ");
+// if(_user==number_){
+//     console.log("congratulation right number")
+// }
+// else if(_user > number_){
+//     console.log("_user is grater than number")
+// }
+// else {
+//     console.log("_user less than number")
+// }
